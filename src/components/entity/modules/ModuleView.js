dbb4dc9ec56781a3b6d39acc41fd2423827b2573
eisahmckoy171-cyber <eisahmckoy171@gmail.com>
2025-11-11 02:22:25 +0000

    Creating a button/button tray and adding/deleting in the CRUDL workflow

--- src/components/entity/modules/ModuleView.js
+++ src/components/entity/modules/ModuleView.js
@@ -1,10 +1,20 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Alert, StyleSheet, Text, View } from "react-native";
 import FullWidthImage from "react-native-fullwidth-image";
+import Icons from "../../UI/Icons";
+import { Button, ButtonTray } from "../../UI/Button";
 
-const ModuleView = ({ module }) => {
+const ModuleView = ({ module, onDelete }) => {
   // Initialisations ---------------------
   // State -------------------------------
   // Handlers ----------------------------
+  const handleDelete = () => onDelete(module);
+
+  const requestDelete = () =>
+    Alert.alert(
+      "Delete warning",
+      `Are you sure that you want to delete module ${module.ModuleCode} ${module.ModuleName}`,
+      [{ text: "Cancel" }, { text: "Delete", onPress: handleDelete }]
+    );
 
   // View --------------------------------
   return (
@@ -23,6 +33,17 @@ const ModuleView = ({ module }) => {
           <Text style={styles.dimText}>(Module Leader)</Text>
         </Text>
       </View>
+
+      <ButtonTray>
+        <Button icon={<Icons.Edit />} label="Modify" />
+        <Button
+          icon={<Icons.Delete />}
+          label="Delete"
+          onClick={requestDelete}
+          styleButton={{ backgroundColor: "mistyrose" }}
+          styleLabel={{ color: "red" }}
+        />
+      </ButtonTray>
     </View>
   );
 };
--- src/components/screens/ModuleListScreen.js
+++ src/components/screens/ModuleListScreen.js
@@ -1,26 +1,48 @@
 import { useState } from "react";
-import { StyleSheet } from "react-native";
+import { LogBox, StyleSheet } from "react-native";
 import Screen from "../layout/Screen";
 import ModuleList from "../entity/modules/ModuleList.js";
-import RenderCount from "../UI/RenderCount.js";
-
+import Icons from "../UI/Icons.js";
+import { Button, ButtonTray } from "../UI/Button.js";
 import initialModules from "../../data/modules.js";
 
 const ModuleListScreen = ({ navigation }) => {
   // Initialisations ---------------------
+  LogBox.ignoreLogs([
+    "non-serializable values were found in the navigation state",
+  ]);
+
   // State -------------------------------
   const [modules, setModules] = useState(initialModules);
 
   // Handlers ----------------------------
-  const handleselct = (module) => navigation.navigate("ModuleViewScreen", { module });
+
   const handleDelete = (module) =>
     setModules(modules.filter((item) => item.ModuleID !== module.ModuleID));
+
+  const handleAdd = (module) => setModules([...modules, module]);
+
+  const onDelete = (module) => {
+    handleDelete(module);
+    navigation.goBack();
+  };
+
+  const onAdd = (module) => {
+    handleAdd(module);
+    navigation.goBack();
+  };
+
+  const gotoViewScreen = (module) =>
+    navigation.navigate("ModuleViewScreen", { module, onDelete });
+  const gotoAddScreen = () => navigation.navigate("ModuleAddScreen", { onAdd });
 
   // View --------------------------------
   return (
     <Screen>
-      <RenderCount />
-      <ModuleList modules={modules} onSelect={handleselct} />
+      <ButtonTray>
+        <Button label="Add" icon={<Icons.Add />} onClick={gotoAddScreen} />
+      </ButtonTray>
+      <ModuleList modules={modules} onSelect={gotoViewScreen} />
     </Screen>
   );
 };
